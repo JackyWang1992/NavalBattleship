cff7ee219f5d6c73f361bd51ca4d556510da5a6d
JackyWang <jackywang@git.com>
2017-12-03 21:29:55 -0500

    wait for cheat
    cheat method comment

--- Launcher.java
+++ Launcher.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Launcher {
     public static void main (String[] args) {
+
+
+
+
+
         NavalBattleship game = new NavalBattleship();
         game.run();
+
     }
+
 }
