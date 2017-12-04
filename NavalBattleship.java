7491ba3dc00119de4c1200f7e276ddde8a92eb47
JackyWang <jackywang@git.com>
2017-12-03 20:13:32 -0500

    version for Qiao

--- NavalBattleship.java
+++ NavalBattleship.java
@@ -3,262 +3,21 @@ import java.util.*;
 public class NavalBattleship {
     private boolean gameContinue = true;
     private boolean roundContinue = true;
-    private char[][] robot_field;
-    private char[][] user_field;
+    private char[][] pcBoard;
+    private char[][] userBoard;
+    private char[][] showPCBoard;
+    //private char[][] uI_field;
     private char seaSymbol = '~';
     private char shipSymbol = 'o';
     private char hitSymbol = '*';
     private char missSymbol = 'x';
     private int[] shipLen = {2, 3, 3, 4, 5};
-
-
-
-    public void run() {
-
-        System.out.println("Welcome to Naval Battle Field!");
-
-        while (gameContinue) {
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            if (input.equals("I don't want to play anymore.")) {
-                break;
-            }
-            else {
-                boolean roundContinue = true;
-                user_field = new char[10][10];
-                robot_field = new char[10][10];
-
-                for (int i = 0; i < robot_field.length; i++) {
-                    for (int j = 0; j < robot_field[0].length; j++) {
-                        robot_field[i][j] = seaSymbol;
-                        user_field[i][j] = seaSymbol;
-                    }
-
-                }
-                printGraph(user_field,robot_field);
-                userStart();
-                setupRobot();
-
-                printGraph(user_field,robot_field);
-                while (roundContinue) {
-                    //userAttack();
-                    //robotAttack();
-                    break;
-                }
-                break;
-            }
-
-
-
-        }
-
-
-
-
-
-
-    }
-    private void setupRobot() {
-        //boolean[][] visited = new boolean[10][10];
-        int count = 0;
-        for (int i = 0; i < shipLen.length; i++) {
-            Random rdm = new Random();
-            boolean contin = false;
-            int x = rdm.nextInt(10);
-            int y = rdm.nextInt(10);
-            List<Integer> posDir = checkValid(shipLen[i], x, y);
-
-            while (robot_field[x][y] == shipSymbol && posDir.size() == 0) {
-
-                x = rdm.nextInt(10);
-                y = rdm.nextInt(10);
-                posDir = checkValid(shipLen[i], x, y);
-            }
-            int dir = rdm.nextInt(4);
-            while (!posDir.contains(dir)) {
-                dir = rdm.nextInt(4);
-            }
-
-            robot_field[x][y] = shipSymbol;
-
-            System.out.println("dir = " + dir);
-            if (dir == 0){
-
-                for (int k = 1; k < shipLen[i]; k++) {
-                    count++;
-
-                    robot_field[x][y - k] = shipSymbol;
-                    //visited[x][y - k] = true;
-
-                }
-
-
-            }
-            //down
-            else if (dir == 1) {
-
-                for (int k = 1; k < shipLen[i]; k++) {
-                    count++;
-
-                    robot_field[x][y + k] = shipSymbol;
-                    //visited[x][y + k] = true;
-
-                }
-
-
-            }
-            //right
-            else if (dir == 2) {
-
-                for (int k = 1; k < shipLen[i]; k++) {
-                    count++;
-
-                    robot_field[x + k][y] = shipSymbol;
-                    //visited[x + k][y] = true;
-
-                }
-            }
-            else if(dir == 3){
-
-                for (int k = 1; k < shipLen[i]; k++) {
-                    count++;
-
-                    robot_field[x - k][y] = shipSymbol;
-                    //visited[x - k][y] = true;
-
-                }
-
-            }
-        }
-        System.out.println("coutn = " + count);
-    }
-    private List<Integer> checkValid(int shipLen, int x, int y) {
-        List<Integer> list = new ArrayList<>();
-        for (int dir = 0; dir < 4; dir++) {
-            if (checkDir(dir, shipLen, x, y))
-                list.add(dir);
-        }
-
-        return list;
-    }
-    private boolean checkDir(int dir, int shipLen, int x, int y) {
-        switch (dir) {
-            //go up
-            case 0: {
-                if (y - shipLen - 1 < 0) return false;
-                for (int k = 1; k < shipLen; k++) {
-
-                    if (robot_field[x][y - k] == shipSymbol)
-                        return false;
-
-                }
-                break;
-            }
-            //down
-            case 1: {
-                if (y + shipLen - 1 >= robot_field.length)
-                    return false;
-                for (int k = 1; k < shipLen; k++) {
-                    if (robot_field[x][y + k] == shipSymbol)
-                        return false;
-
-                }
-                break;
-            }
-            //right
-            case 2: {
-                if (x + shipLen - 1 >= robot_field.length)
-                    return false;
-                for (int k = 1; k < shipLen; k++) {
-
-                    if (robot_field[x + k][y] == shipSymbol)
-                        return false;
-
-                }
-                break;
-
-            }
-            case 3: {
-                if (x - shipLen - 1 < 0) return false;
-                for (int k = 1; k < shipLen; k++) {
-
-                    if (robot_field[x - k][y] == shipSymbol)
-                        return false;
-
-                }
-                break;
-            }
-        }
-        return true;
-    }
-
-    private static void printGraph(char[][] userBoard, char[][] pcBoard){
-        System.out.print("   ");
-        for (int i = 1; i <= 10; i++){
-            System.out.print(i);
-            System.out.print(" ");
-        }
-        System.out.println();
-        for (int i = 0; i < pcBoard.length; i++){
-            System.out.print(i + 1);
-            System.out.print(" ");
-            System.out.print(" ");
-            for (int j = 0; j < pcBoard[i].length; j++){
-                System.out.print(pcBoard[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-        for (int i = 0; i < 25; i++){
-            System.out.print("-");
-        }
-        System.out.println();
-        System.out.println();
-        System.out.print("   ");
-        for (int i = 1; i <= 10; i++){
-            System.out.print(i);
-            System.out.print(" ");
-        }
-        System.out.println();
-        for (int i = 0; i < userBoard.length; i++){
-            System.out.print(i + 1);
-            System.out.print(" ");
-            System.out.print(" ");
-            for (int j = 1; j < userBoard[i].length; j++){
-                System.out.print(userBoard[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-    }
-
-
-    private void userStart() {
-
-    }
-    private void userAttack() {
-
-    }
-    private void robotAttack() {
-        char[][] visited = new char[10][10];
-        List<int[]> tryFirst = new ArrayList<>();
-        List<int[]> hitShip = new ArrayList<>();
-        Random rdm = new Random();
-        if (hitShip.size() == 0) {import java.util.*;
-
-public class NavalBattleship {
-    private boolean gameContinue = true;
-    private boolean roundContinue = true;
-    private char[][] robot_field;
-    private char[][] user_field;
-    private char[][] uI_field;
-    private char seaSymbol = '~';
-    private char shipSymbol = 'o';
-    private char hitSymbol = '*';
-    private char missSymbol = 'x';
-    private int[] shipLen = {2, 3, 3, 4, 5};
+    //private int[] shipLen = {2};
     private int totalPoints = 17;
+    static ArrayList<Integer> size;
+
+
+
 
 
 
@@ -276,28 +35,29 @@ public class NavalBattleship {
             }
             else {
                 boolean roundContinue = true;
-                user_field = new char[10][10];
-                robot_field = new char[10][10];
-                uI_field = new char[10][10];
+                userBoard = new char[10][10];
+                pcBoard = new char[10][10];
+                showPCBoard = new char[10][10];
 
 
-                for (int i = 0; i < robot_field.length; i++) {
-                    for (int j = 0; j < robot_field[0].length; j++) {
-                        robot_field[i][j] = seaSymbol;
-                        user_field[i][j] = seaSymbol;
-                        uI_field[i][j] = seaSymbol;
+                for (int i = 0; i < pcBoard.length; i++) {
+                    for (int j = 0; j < pcBoard[0].length; j++) {
+                        pcBoard[i][j] = seaSymbol;
+                        userBoard[i][j] = seaSymbol;
+                        showPCBoard[i][j] = seaSymbol;
                     }
 
                 }
-                printGraph(user_field,robot_field);
-                userStart();
+                printGraph(userBoard, pcBoard);
+                userStart(); //call
                 setupRobot();
 
-                printGraph(user_field,robot_field);
+                printGraph(userBoard, pcBoard);
                 while (roundContinue) {
-                    //userAttack();
+                    userAttack();
                     //robotAttack();
-                    break;
+                    if (isEnd(userBoard))
+                        break;
                 }
 
             }
@@ -322,7 +82,7 @@ public class NavalBattleship {
             int y = rdm.nextInt(10);
             List<Integer> posDir = checkValid(shipLen[i], x, y);
 
-            while (robot_field[x][y] == shipSymbol && posDir.size() == 0) {
+            while (pcBoard[x][y] == shipSymbol && posDir.size() == 0) {
 
                 x = rdm.nextInt(10);
                 y = rdm.nextInt(10);
@@ -333,14 +93,14 @@ public class NavalBattleship {
                 dir = rdm.nextInt(4);
             }
 
-            robot_field[x][y] = shipSymbol;
+            pcBoard[x][y] = shipSymbol;
 
             if (dir == 0){
 
                 for (int k = 1; k < shipLen[i]; k++) {
                     count++;
 
-                    robot_field[x][y - k] = shipSymbol;
+                    pcBoard[x][y - k] = shipSymbol;
                     //visited[x][y - k] = true;
 
                 }
@@ -353,7 +113,7 @@ public class NavalBattleship {
                 for (int k = 1; k < shipLen[i]; k++) {
                     count++;
 
-                    robot_field[x][y + k] = shipSymbol;
+                    pcBoard[x][y + k] = shipSymbol;
                     //visited[x][y + k] = true;
 
                 }
@@ -366,7 +126,7 @@ public class NavalBattleship {
                 for (int k = 1; k < shipLen[i]; k++) {
                     count++;
 
-                    robot_field[x + k][y] = shipSymbol;
+                    pcBoard[x + k][y] = shipSymbol;
                     //visited[x + k][y] = true;
 
                 }
@@ -376,7 +136,7 @@ public class NavalBattleship {
                 for (int k = 1; k < shipLen[i]; k++) {
                     count++;
 
-                    robot_field[x - k][y] = shipSymbol;
+                    pcBoard[x - k][y] = shipSymbol;
                     //visited[x - k][y] = true;
 
                 }
@@ -401,7 +161,7 @@ public class NavalBattleship {
                 if (y - shipLen - 1 < 0) return false;
                 for (int k = 1; k < shipLen; k++) {
 
-                    if (robot_field[x][y - k] == shipSymbol)
+                    if (pcBoard[x][y - k] == shipSymbol)
                         return false;
 
                 }
@@ -409,10 +169,10 @@ public class NavalBattleship {
             }
             //down
             case 1: {
-                if (y + shipLen - 1 >= robot_field.length)
+                if (y + shipLen - 1 >= pcBoard.length)
                     return false;
                 for (int k = 1; k < shipLen; k++) {
-                    if (robot_field[x][y + k] == shipSymbol)
+                    if (pcBoard[x][y + k] == shipSymbol)
                         return false;
 
                 }
@@ -420,11 +180,11 @@ public class NavalBattleship {
             }
             //right
             case 2: {
-                if (x + shipLen - 1 >= robot_field.length)
+                if (x + shipLen - 1 >= pcBoard.length)
                     return false;
                 for (int k = 1; k < shipLen; k++) {
 
-                    if (robot_field[x + k][y] == shipSymbol)
+                    if (pcBoard[x + k][y] == shipSymbol)
                         return false;
 
                 }
@@ -435,7 +195,7 @@ public class NavalBattleship {
                 if (x - shipLen - 1 < 0) return false;
                 for (int k = 1; k < shipLen; k++) {
 
-                    if (robot_field[x - k][y] == shipSymbol)
+                    if (pcBoard[x - k][y] == shipSymbol)
                         return false;
 
                 }
@@ -460,7 +220,7 @@ public class NavalBattleship {
     }
 
     public static void printSea(char[][] board){
-        System.out.print("y:x");
+        System.out.print("x:y");
         for (int i = 1; i <= 10; i++){
             System.out.print(i);
             System.out.print(" ");
@@ -488,10 +248,116 @@ public class NavalBattleship {
 
 
     private void userStart() {
+        size = new ArrayList<>();
+        // set up the sea char
+        int count = 0;
+        boolean fail;
+        size.add(2);
+        size.add(3);
+        size.add(3);
+        size.add(4);
+        size.add(5);
 
+        for (int i = 0; i < userBoard.length; i++){
+            for (int j = 0; j < userBoard[i].length; j++){
+                userBoard[i][j] = seaSymbol;
+            }
+        }
+
+        for (int i = 0; i < showPCBoard.length; i++){
+            for (int j = 0; j < showPCBoard[i].length; j++){
+                showPCBoard[i][j] = seaSymbol;
+            }
+        }
+
+        do {
+            fail = false;
+            Scanner sc = new Scanner(System.in);
+            int[][] location = new int[2][2];
+
+            for (int i = 0; i < location.length; i++) {
+                System.out.printf("Please enter one end of ship of size %d:%n", size.get(0));
+                do {
+                    System.out.println("x: ");
+                    location[i][0] = sc.nextInt();
+                    if (location[i][0] < 1 || location[i][0] > 10) {
+                        System.out.println("please enter again!");
+                    }
+                } while (location[i][0] < 1 || location[i][0] > 10);
+                do {
+                    System.out.println("y: ");
+                    location[i][1] = sc.nextInt();
+                    if (location[i][1] < 1 || location[i][1] > 10) {
+                        System.out.println("please enter again!");
+                    }
+                } while (location[i][1] < 1 || location[i][1] > 10);
+            }
+
+            if (location[1][0] == location[0][0]) {
+                int length = location[1][1] - location[0][1] + 1;
+
+                if (length != size.get(0)) {
+                    System.out.println("please re-enter the location");
+                    continue;
+                }
+
+                for (int k = location[0][1]; k <= location[1][1] ; k++) {
+                    if (userBoard[location[0][0] - 1][k - 1] == shipSymbol) {
+                        System.out.println("please re-enter the location");
+                        fail = true;
+                        break;
+                    }
+                    userBoard[location[0][0] - 1][k - 1] = shipSymbol;
+                }
+
+            } else {
+                int length = location[1][0] - location[0][0] + 1;
+                if (length != size.get(0)) {
+                    System.out.println("please re-enter the location");
+                    continue;
+                }
+                for (int l = location[0][0]; l <= location[1][0] ; l++) {
+                    if (userBoard[l - 1][location[0][1] - 1] == shipSymbol) {
+                        System.out.println("please re-enter the location");
+                        fail = true;
+                        break;
+                    }
+                    userBoard[l - 1][location[0][1] - 1] = shipSymbol;
+                }
+            }
+            if (fail) {
+                continue;
+            }
+            printGraph(userBoard, pcBoard);
+            size.remove(0);
+            count++;
+        } while (count < shipLen.length || fail == true);
+        // set up the location
+        System.out.println("Baby! You are all set! ");
+        //userAttack();
+        //char[][] initialPcBoard, char[][] userBoard, char[][] pcBoard
     }
     private void userAttack() {
+        Scanner sc = new Scanner(System.in);
+        boolean isBoat = true;
+        while (isBoat){
+            System.out.println("Please enter the x coordinate you want to attack :\n");
+            int x = sc.nextInt() - 1;
+            System.out.println("Please enter the y coordinate you want to attack :\n");
+            int y = sc.nextInt() - 1;
 
+
+            if (pcBoard[x][y] == shipSymbol){
+                showPCBoard[x][y] = hitSymbol;
+            } else if (pcBoard[x][y] == seaSymbol){
+                showPCBoard[x][y] = missSymbol;
+                isBoat = false;
+                // if (isEnd()){
+                //   break;
+                // }
+            }
+            printGraph(userBoard, showPCBoard);
+        }
     }
 
 
@@ -506,20 +372,5 @@ public class NavalBattleship {
         }
         return count == totalPoints ? true : false;
     }
-
-}
-
-            int x = rdm.nextInt(10);
-            int y = rdm.nextInt(10);
-            if (robot_field[x][y] == shipSymbol) {
-
-            }
-        }
-
-    }
-    private void helper() {
-
-    }
-
 
 }
