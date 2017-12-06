f7f1a6a54e046e42cdca19555c21171035acb1b5
JackyWang <jackywang@git.com>
2017-12-05 20:36:19 -0500

    bug free version
    by JackyWang

--- set/UserSetUp.java
+++ set/UserSetUp.java
@@ -10,10 +10,11 @@ import javax.print.attribute.standard.MediaSize;
 import print.PrintGraph;
 
 public class UserSetUp {
-
+    private static ArrayList<Integer> size;
+    private static Scanner sc;
 
     public void userStart(){
-        ArrayList<Integer> size = new ArrayList<>();
+        size = new ArrayList<>();
         // set up the sea char
         int count = 0;
         boolean fail;
@@ -23,81 +24,15 @@ public class UserSetUp {
         size.add(4);
         size.add(5);
 
-        NavalBattleship.userBoard = setUpSea(NavalBattleship.userBoard);
+        setUpSea(NavalBattleship.userBoard);
 
-        NavalBattleship.showPCBoard = setUpSea(NavalBattleship.showPCBoard);
-
-        // for (int i = 0; i < NavalBattleship.userBoard.length; i++){
-        //     for (int j = 0; j < NavalBattleship.userBoard[i].length; j++){
-        //         NavalBattleship.userBoard[i][j] = NavalBattleship.seaSymbol;
-        //     }
-        // }
-        //
-        // for (int i = 0; i < NavalBattleship.showPCBoard.length; i++){
-        //     for (int j = 0; j < NavalBattleship.showPCBoard[i].length; j++){
-        //         NavalBattleship.showPCBoard[i][j] = NavalBattleship.seaSymbol;
-        //     }
-        // }
+        setUpSea(NavalBattleship.showPCBoard);
 
         do {
             fail = false;
-            Scanner sc = new Scanner(System.in);
+            sc = new Scanner(System.in);
             int[][] location = new int[2][2];
-
-            // for (int i = 0; i < location.length; i++) {
-            //     System.out.printf("Please enter one end of ship of size %d:%n", size.get(0));
-            //     do {
-            //         System.out.println("x: ");
-            //         location[i][0] = sc.nextInt();
-            //         if (location[i][0] < 1 || location[i][0] > 10) {
-            //             System.out.println("please enter again!");
-            //         }
-            //     } while (location[i][0] < 1 || location[i][0] > 10);
-            //     do {
-            //         System.out.println("y: ");
-            //         location[i][1] = sc.nextInt();
-            //         if (location[i][1] < 1 || location[i][1] > 10) {
-            //             System.out.println("please enter again!");
-            //         }
-            //     } while (location[i][1] < 1 || location[i][1] > 10);
-            // }
-
             location = setUpLocation(location);
-
-            // if (location[1][0] == location[0][0]) {
-            //     int length = location[1][1] - location[0][1] + 1;
-            //
-            //
-            //     if (length != size.get(0)) {
-            //         System.out.println("please re-enter the location");
-            //         continue;
-            //     }
-            //
-            //     for (int k = location[0][1]; k <= location[1][1] ; k++) {
-            //         if (NavalBattleship.userBoard[location[0][0] - 1][k - 1] == NavalBattleship.shipSymbol) {
-            //             System.out.println("please re-enter the location");
-            //             fail = true;
-            //             break;
-            //         }
-            //         NavalBattleship.userBoard[location[0][0] - 1][k - 1] = NavalBattleship.shipSymbol;
-            //     }
-            //
-            // } else {
-            //     int length = location[1][0] - location[0][0] + 1;
-            //     if (length != size.get(0)) {
-            //         System.out.println("please re-enter the location");
-            //         continue;
-            //     }
-            //     for (int l = location[0][0]; l <= location[1][0] ; l++) {
-            //         if (NavalBattleship.userBoard[l - 1][location[0][1] - 1] == NavalBattleship.shipSymbol) {
-            //             System.out.println("please re-enter the location");
-            //             fail = true;
-            //             break;
-            //         }
-            //         NavalBattleship.userBoard[l - 1][location[0][1] - 1] = NavalBattleship.shipSymbol;
-            //     }
-            // }
-
             fail = setUpOneShip(NavalBattleship.userBoard, location, fail);
             if (fail) {
                 continue;
@@ -118,27 +53,42 @@ public class UserSetUp {
               board[i][j] = NavalBattleship.seaSymbol;
           }
       }
-      return board;
     }
 
     public static int[][] setUpLocation(int[][] location){
 
       for (int i = 0; i < location.length; i++) {
-          System.out.printf("Please enter one end of ship of size %d:%n", size.get(0));
+          if (i == 0) {
+            System.out.printf("Please enter the head of ship of size %d:%n", size.get(0));
+          } else {
+            System.out.printf("Please enter the tail of ship of size %d:%n", size.get(0));
+          }
+          boolean isContinue;
           do {
+              isContinue = false;
               System.out.println("x: ");
-              location[i][0] = sc.nextInt();
+              try {
+                location[i][0] = sc.nextInt();
+              } catch (Exception e) {
+                sc.next();
+                isContinue = true;
+              }
               if (location[i][0] < 1 || location[i][0] > 10) {
                   System.out.println("please enter again!");
               }
-          } while (location[i][0] < 1 || location[i][0] > 10);
+          } while (location[i][0] < 1 || location[i][0] > 10 || isContinue);
           do {
               System.out.println("y: ");
-              location[i][1] = sc.nextInt();
+              try {
+                location[i][1] = sc.nextInt();
+              } catch (Exception e) {
+                sc.next();
+                isContinue = true;
+              }
               if (location[i][1] < 1 || location[i][1] > 10) {
                   System.out.println("please enter again!");
               }
-          } while (location[i][1] < 1 || location[i][1] > 10);
+          } while (location[i][1] < 1 || location[i][1] > 10 || isContinue == true);
       }
       return location;
     }
@@ -147,8 +97,6 @@ public class UserSetUp {
 
       if (location[1][0] == location[0][0]) {
           int length = location[1][1] - location[0][1] + 1;
-
-
           if (length != size.get(0)) {
               System.out.println("please re-enter the location");
               fail = true;
@@ -180,8 +128,7 @@ public class UserSetUp {
               userBoard[l - 1][location[0][1] - 1] = NavalBattleship.shipSymbol;
           }
       }
+      return fail;
     }
-
-
 
   }
