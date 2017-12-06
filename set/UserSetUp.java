49c3986a05039d6cdd0b9abc59f14ab7402a6993
JackyWang <jackywang@git.com>
2017-12-06 01:14:14 -0500

    add more method for usersetup
    by JackyWang

--- set/UserSetUp.java
+++ set/UserSetUp.java
@@ -26,6 +26,7 @@ public class UserSetUp {
         // set up the sea char
         int count = 0;
         boolean fail;
+        // assign ship sizes
         size.add(2);
         size.add(3);
         size.add(3);
@@ -33,7 +34,6 @@ public class UserSetUp {
         size.add(5);
         // call the setUpSea method for userBoard and showPCBoard
         setUpSea(NavalBattleship.userBoard);
-
         setUpSea(NavalBattleship.showPCBoard);
         // start to put five different ships
         do {
@@ -74,45 +74,70 @@ public class UserSetUp {
      @return the 2D array of location which represent the ship's location
     */
     public static int[][] setUpLocation(int[][] location){
-
+      // set up one ship coordinate
       for (int i = 0; i < location.length; i++) {
           if (i == 0) {
             System.out.printf("Please enter the head of ship of size %d:%n", size.get(0));
           } else {
             System.out.printf("Please enter the tail of ship of size %d:%n", size.get(0));
           }
-          boolean isContinue;
-          // set up the ship coordinates
-          // x
-          do {
-              isContinue = false;
-              System.out.println("x: ");
-              // add try ... catch... to handle non-integer
-              try {
-                location[i][0] = sc.nextInt();
-              } catch (Exception e) {
-                sc.next();
-                isContinue = true;
-              }
-              if (location[i][0] < 1 || location[i][0] > 10) {
-                  System.out.println("please enter again!");
-              }
-          } while (location[i][0] < 1 || location[i][0] > 10 || isContinue);
-          // y
-          do {
-              System.out.println("y: ");
-              try {
-                location[i][1] = sc.nextInt();
-              } catch (Exception e) {
-                sc.next();
-                isContinue = true;
-              }
-              if (location[i][1] < 1 || location[i][1] > 10) {
-                  System.out.println("please enter again!");
-              }
-          } while (location[i][1] < 1 || location[i][1] > 10 || isContinue == true);
+          // set up x coordinate
+          location[i][0] = setUpX(location, i);
+          // set up y coordinate
+          location[i][1] = setUpY(location, i);
       }
       return location;
+    }
+
+    /**
+      setup the x coordinate of the location
+      @param location the 2D array of integers represent the location of the ship
+      @param i the index of the location
+      @return an integer represent the x coordinate
+    */
+    public static int setUpX (int[][] location, int i){
+      boolean isContinue;
+      do {
+          isContinue = false;
+          System.out.println("x: ");
+          // add try ... catch... to handle non-integer
+          try {
+            location[i][0] = sc.nextInt();
+          } catch (Exception e) {
+            sc.next();
+            isContinue = true;
+          }
+          if (location[i][0] < 1 || location[i][0] > 10) {
+              System.out.println("please enter again!");
+          }
+      } while (location[i][0] < 1 || location[i][0] > 10 || isContinue);
+
+      return location[i][0];
+    }
+
+    /**
+      setup the x coordinate of the location
+      @param location the 2D array of integers represent the location of the ship
+      @param i the index of the location
+      @return an integer represent the y coordinate
+    */
+    public static int setUpY (int[][] location, int i){
+      boolean isContinue;
+      do {
+          isContinue = false;
+          System.out.println("y: ");
+          try {
+            location[i][1] = sc.nextInt();
+          } catch (Exception e) {
+            sc.next();
+            isContinue = true;
+          }
+          if (location[i][1] < 1 || location[i][1] > 10) {
+              System.out.println("please enter again!");
+          }
+      } while (location[i][1] < 1 || location[i][1] > 10 || isContinue == true);
+
+      return location[i][1];
     }
 
     /**
@@ -127,13 +152,8 @@ public class UserSetUp {
       // when x is equal
       if (location[1][0] == location[0][0]) {
           int length = location[1][1] - location[0][1] + 1;
-          // when two ships' location conflicts, re-enter
-          if (length != size.get(0)) {
-              System.out.println("please re-enter the location");
-              fail = true;
-              return fail;
-          }
-
+          fail = isSize(length);
+          // check out if two ships has the same coordinate
           for (int k = location[0][1]; k <= location[1][1] ; k++) {
               if (userBoard[location[0][0] - 1][k - 1] == NavalBattleship.shipSymbol) {
                   System.out.println("please re-enter the location");
@@ -146,11 +166,8 @@ public class UserSetUp {
       } else {
         //when y is equal
           int length = location[1][0] - location[0][0] + 1;
-          if (length != size.get(0)) {
-              System.out.println("please re-enter the location");
-              fail = true;
-              return fail;
-          }
+          fail = isSize(length);
+          //checkout if two boats have the same coordinates
           for (int l = location[0][0]; l <= location[1][0] ; l++) {
               if (userBoard[l - 1][location[0][1] - 1] == NavalBattleship.shipSymbol) {
                   System.out.println("please re-enter the location");
@@ -159,6 +176,21 @@ public class UserSetUp {
               }
               userBoard[l - 1][location[0][1] - 1] = NavalBattleship.shipSymbol;
           }
+      }
+      return fail;
+    }
+
+    /**
+     this method check whether the user enter the right size of ship
+     @param length the length of ship from user's input
+     @return a boolean whether the user enter the right size
+    */
+    public static boolean isSize(int length) {
+      boolean fail = false;
+      // when two ships' location conflicts, re-enter
+      if (length != size.get(0)) {
+          System.out.println("please re-enter the location");
+          fail = true;
       }
       return fail;
     }
